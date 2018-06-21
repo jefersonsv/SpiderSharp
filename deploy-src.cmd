e43427a88406fa9fba1a1f3f0cd84b1e34f492d7
Jeferson Tenorio <jefersonsv@gmail.com>
2018-06-21 15:25:20 -0300

    fix broken pieces

--- deploy-src.cmd
+++ deploy-src.cmd
@@ -1,5 +1,7 @@
 REM https://docs.microsoft.com/pt-br/dotnet/core/tools/custom-templates
 rd /s /q .\src\SpiderSharp\bin\Release
-dotnet build -c Release .\src\SpiderSharp\SpiderSharp.csproj
-nuget push .\src\SpiderSharp\bin\Release\*.nupkg -Source https://api.nuget.org/v3/index.json
-del /f /s /q .\src\SpiderSharp\bin\Release\*.nupkg
+REM dotnet build -c Release .\src\SpiderSharp\SpiderSharp.csproj
+dotnet build -c Release .\SpiderSharp.sln
+nuget pack SpiderSharp.nuspec
+nuget push .\*.nupkg -Source https://api.nuget.org/v3/index.json
+del /f /s /q .\*.nupkg
---
+++ sample/ScrapQuotes/readme.md
@@ -0,0 +1,24 @@
+﻿# Scrap Quotes
+
+The Simple quotes scrap of http://quotes.toscrape.com for tutorial purpose
+
+# Scraped data
+```
+[
+	{
+		'text': '"The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking"',
+		'author': 'by Albert Einstein (about)',
+		'tags': ['change', 'deep-thoughts', 'thinking', 'world']
+	},
+	{
+		...
+	}
+]
+```
+
+# Running the spider
+
+Run the spider executing the application:
+```sh
+$ ScrapQuotes
+```
