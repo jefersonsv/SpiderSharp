2ee61d064d30865f1b665d67cc5ee2e6e989ab6a
Jeferson Tenorio <jefersonsv@gmail.com>
2019-03-12 10:18:00 -0300

    improve scrapshell command menu

---
+++ app/ScrapShell/CommandMenu.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ScrapShell
+{
+    class CommandItem
+    {
+        public Regex Regex { get; set; }
+        public Action<Match> Action { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class CommandMenu
+    {
+        Dictionary<string, CommandItem> dict;
+        string prefix;
+        public delegate void CommandNotFound();
+        public event CommandNotFound OnCommandNotFound;
+
+        public CommandMenu(string menuName)
+        {
+            prefix = menuName;
+            dict = new Dictionary<string, CommandItem>();
+        }
+
+        private void CommandMenu_OnCommandNotFound()
+        {
+            Console.WriteLine("Command not found");
+        }
+
+        public void AddCommand(string regex, Action<Match> action, string description = null)
+        {
+            if (action == null)
+                throw new Exception("The action cannot be null");
+
+            dict.Add(regex, new CommandItem { Regex = new Regex(regex, RegexOptions.IgnoreCase), Action = action, Description = description });
+        }
+
+        public void AddExitCommand(string regex, string description = null)
+        {
+            dict.Add(regex, new CommandItem { Regex = new Regex(regex, RegexOptions.IgnoreCase), Action = null, Description = description });
+        }
+
+        public void PrintHelp(char separator = '=')
+        {
+            if (!string.IsNullOrEmpty(prefix))
+            {
+                Console.WriteLine(prefix);
+                Console.WriteLine(new String(separator, prefix.Length));
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Value.Description ?? item.Key);
+            }
+        }
+
+        public void Execute()
+        {
+            ReadLine.HistoryEnabled = true;
+
+            while (true)
+            {
+                var isValid = false;
+                Console.WriteLine(string.Empty);
+                string input = ReadLine.Read($"{prefix} # ");
+
+                foreach (var item in dict)
+                {
+                    if (item.Value.Regex.IsMatch(input))
+                    {
+                        isValid = true;
+                        if (item.Value.Action == null)
+                            return;
+
+                        var match = item.Value.Regex.Match(input);
+                        try
+                        {
+                            item.Value.Action(match);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+
+                        break;
+                    }
+                }
+
+                if (!isValid)
+                {
+                    if (OnCommandNotFound == null)
+                        OnCommandNotFound += CommandMenu_OnCommandNotFound;
+
+                    OnCommandNotFound();
+                }
+            }
+        }
+    }
+}
--- app/ScrapShell/Program.cs
+++ app/ScrapShell/Program.cs
@@ -11,245 +11,148 @@ namespace ScrapShell
 {
     public class Program
     {
-        static readonly Regex loadRegex = new Regex("load (?<url>.*)");
-        static readonly Regex setRegex = new Regex("set (?<driver>.*)");
-        static readonly Regex saveRegex = new Regex("save (?<filename>.*)");
-        static readonly Regex innertext = new Regex("innertext (?<selector>.*)");
-        static readonly Regex innerhtml = new Regex("innerhtml (?<selector>.*)");
-        static readonly Regex links = new Regex("links (?<selector>.*)");
-        static readonly Regex browseRegex = new Regex("browse");
-        static readonly Regex quitRegex = new Regex("quit");
-        static readonly Regex helpRegex = new Regex("help");
         static HttpRequester.Requester requester;
+        static string content;
+        static CommandMenu currentMenu;
 
-        private static void Main(string[] args)
+        static void LoadCommand(Match match)
         {
-            //ReadLine.HistoryEnabled = true;
+            var url = match.Groups["url"].Value;
 
-            //DrawMainMenu();
-            //return;
-
-
-            if (args == null || !args.Any())
+            if (File.Exists(url))
             {
-                ReadLine.HistoryEnabled = true;
-
-                // https://github.com/tonerdo/readline
-
-                var content = string.Empty;
-
-                Console.Clear();
-                PrintCommands();
-                while (true)
-                {
-                    string input = ReadLine.Read("# ");
-                    if (string.IsNullOrWhiteSpace(input))
-                        continue;
-
-                    Console.WriteLine(string.Empty);
-                    // Console.WriteLine("Type command: ");
-
-                    if (loadRegex.IsMatch(input))
-                    {
-                        var url = loadRegex.Match(input).Groups["url"].Value;
-
-                        if (File.Exists(url))
-                        {
-                            content = System.IO.File.ReadAllText(url);
-                        }
-                        else
-                        {
-                            if (requester != null)
-                            {
-                                content = requester.GetContentAsync(url).Result;
-                            }
-                            else
-                            {
-                                HttpRequester.ChromeHeadlessPersistentClient httpRequester = new HttpRequester.ChromeHeadlessPersistentClient();
-                                content = httpRequester.GoAndGetContentAsync(url).Result;
-                            }
-
-                            var tmp = System.IO.Path.GetTempFileName() + ".html";
-                            System.IO.File.WriteAllText(tmp, content);
-                            Console.WriteLine($"Content saved: {tmp}");
-                        }
-
-                        Console.WriteLine($"{content.Count()} bytes loaded");
-                        continue;
-                    }
-
-                    if (setRegex.IsMatch(input))
-                    {
-                        var driver = setRegex.Match(input).Groups["driver"].Value;
-                        if (string.IsNullOrEmpty(driver))
-                        {
-                            Console.WriteLine("You must type on valid driver");
-                            continue;
-                        }
-
-                        switch (driver.ToLower().Trim())
-                        {
-                            case "anglesharp":
-                                requester = new HttpRequester.Requester(HttpRequester.EnumHttpProvider.AngleSharp);
-                                Console.WriteLine("AngleSharp has been set");
-                                break;
-                        }
-
-                        continue;
-                    }
-
-                    if (innertext.IsMatch(input))
-                    {
-                        var selector = innertext.Match(input).Groups["selector"].Value;
-                        if (string.IsNullOrEmpty(selector))
-                        {
-                            Console.WriteLine("You must type the selector");
-                            continue;
-                        }
-
-                        Nodes nodes = new Nodes(content);
-                        var res = nodes.SelectNodes(selector);
-                        res.ToList().ForEach(a => Console.WriteLine(a.GetInnerText()));
-
-                        continue;
-                    }
-
-                    if (innerhtml.IsMatch(input))
-                    {
-                        var selector = innerhtml.Match(input).Groups["selector"].Value;
-                        if (string.IsNullOrEmpty(selector))
-                        {
-                            Console.WriteLine("You must type the selector");
-                            continue;
-                        }
-
-                        Nodes nodes = new Nodes(content);
-                        var res = nodes.SelectNodes(selector);
-                        res.ToList().ForEach(a => Console.WriteLine(a.GetInnerHtml()));
-
-                        continue;
-                    }
-
-                    if (links.IsMatch(input))
-                    {
-                        var selector = links.Match(input).Groups["selector"].Value;
-                        if (string.IsNullOrEmpty(selector))
-                        {
-                            Console.WriteLine("You must type the selector");
-                            continue;
-                        }
-
-                        Nodes nodes = new Nodes(content);
-                        var res = nodes.SelectHref(selector);
-                        res.ToList().ForEach(a => Console.WriteLine(a));
-
-                        continue;
-                    }
-
-                    if (saveRegex.IsMatch(input))
-                    {
-                        var filename = saveRegex.Match(input).Groups["filename"].Value;
-                        if (string.IsNullOrEmpty(content))
-                        {
-                            Console.WriteLine("You must load some url before execute save command");
-                            continue;
-                        }
-
-                        System.IO.File.WriteAllText(filename, content);
-
-                        Console.WriteLine("Saved");
-                        continue;
-                    }
-
-                    if (browseRegex.IsMatch(input))
-                    {
-                        if (string.IsNullOrEmpty(content))
-                        {
-                            Console.WriteLine("You must load some content before execute open command");
-                            continue;
-                        }
-
-                        var tmp = System.IO.Path.GetTempFileName() + ".html";
-                        System.IO.File.WriteAllText(tmp, content);
-                        OpenBrowser(tmp);
-
-                        Console.WriteLine("Browser opened");
-                        continue;
-                    }
-
-                    if (helpRegex.IsMatch(input))
-                    {
-                        PrintCommands();
-                        continue;
-                    }
-
-                    if (quitRegex.IsMatch(input))
-                    {
-                        Console.WriteLine("Exiting");
-                        break;
-                    }
-
-                    Console.WriteLine("Command not found");
-                }
+                content = System.IO.File.ReadAllText(url);
             }
-        }
-
-        private static void DrawMainMenu()
-        {
-            var menu = string.Empty;
-
-            do
+            else
             {
-                Console.Clear();
-                menu = new Menu().Render(new string[] { "Set Engine", "Load Page", "Save to disk", "Browse", "Help", "Quit" });
-                switch (menu)
+                if (requester != null)
                 {
-                    case "Set Engine":
-                        DrawSetEnginer();
-                        break;
-
-                    case "Load Page":
-                        DrawLoadPage();
-                        break;
+                    content = requester.GetContentAsync(url).Result;
                 }
-            } while (menu != "Quit");
+                else
+                {
+                    HttpRequester.ChromeHeadlessPersistentClient httpRequester = new HttpRequester.ChromeHeadlessPersistentClient();
+                    content = httpRequester.GoAndGetContentAsync(url).Result;
+                }
+
+                var tmp = System.IO.Path.GetTempFileName() + ".html";
+                System.IO.File.WriteAllText(tmp, content);
+                Console.WriteLine($"Content saved: {tmp}");
+            }
+
+            Console.WriteLine($"{content.Count()} bytes loaded");
         }
 
-        private static void DrawSetEnginer()
+        static void SetCommand(Match match)
         {
-            Console.Clear();
-            var menu = new Menu().Render(new string[] { "Set AngleSharp", "Set WebClient", "Back" });
-            switch (menu)
+            switch (match.Groups["driver"].Value)
             {
-                case "Set AngleSharp":
+                case "anglesharp":
                     requester = new HttpRequester.Requester(HttpRequester.EnumHttpProvider.AngleSharp);
-                    return;
-                case "Set WebClient":
-                    requester = new HttpRequester.Requester(HttpRequester.EnumHttpProvider.WebClient);
-                    return;
+                    Console.WriteLine("AngleSharp has been set");
+                    break;
             }
         }
 
-        private static void DrawLoadPage()
+        static void SaveCommand(Match match)
         {
-            Console.Clear();
-            Console.Write("Enter url: ");
-            var url = ReadLine.Read("# ");
+            var filename = match.Groups["filename"].Value;
 
-            var content = requester.GetContentAsync(url).Result;
+            if (string.IsNullOrEmpty(content))
+            {
+                Console.WriteLine("You must load some url before execute save command");
+            }
+            else
+            {
+                System.IO.File.WriteAllText(filename, content);
+                Console.WriteLine("Saved");
+            }
         }
 
-        static void PrintCommands()
+        static void HelpCommand(Match match)
         {
-            Console.WriteLine("load => load url or local file");
-            Console.WriteLine("save => save content to local file");
-            Console.WriteLine("set => set HttpRequest that you want");
-            Console.WriteLine("innertext => select innertext using css selector");
-            Console.WriteLine("innerhtml => select innerhtml using css selector");
-            Console.WriteLine("links => select links using css selector");
-            Console.WriteLine("browse => open browser with content");
-            Console.WriteLine("quit => exit program");
-            Console.WriteLine("help => show all commands");
+            currentMenu.PrintHelp();
+        }
+
+        static void BrowseCommand(Match match)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                Console.WriteLine("You must load some content before execute open command");
+            }
+            else
+            {
+                var tmp = System.IO.Path.GetTempFileName() + ".html";
+                System.IO.File.WriteAllText(tmp, content);
+                OpenBrowser(tmp);
+
+                Console.WriteLine("Browser opened");
+            }
+        }
+
+        static void InnerTextCommand(Match match)
+        {
+            var selector = match.Groups["selector"].Value;
+            if (string.IsNullOrEmpty(selector))
+            {
+                Console.WriteLine("You must type the selector");
+            }
+            else
+            {
+                Nodes nodes = new Nodes(content);
+                var res = nodes.SelectNodes(selector);
+                res.ToList().ForEach(a => Console.WriteLine(a.GetInnerText()));
+                Console.WriteLine($"{res.ToList().Count} itens found");
+            }
+        }
+
+        static void InnerHtmlCommand(Match match)
+        {
+            var selector = match.Groups["selector"].Value;
+            if (string.IsNullOrEmpty(selector))
+            {
+                Console.WriteLine("You must type the selector");
+            }
+            else
+            {
+                Nodes nodes = new Nodes(content);
+                var res = nodes.SelectNodes(selector);
+                res.ToList().ForEach(a => Console.WriteLine(a.GetInnerHtml()));
+                Console.WriteLine($"{res.ToList().Count} itens found");
+            }
+        }
+
+        static void LinksCommand(Match match)
+        {
+            var selector = match.Groups["selector"].Value;
+            if (string.IsNullOrEmpty(selector))
+            {
+                Console.WriteLine("You must type the selector");
+            }
+            else
+            {
+                Nodes nodes = new Nodes(content);
+                var res = nodes.SelectHref(selector);
+                res.ToList().ForEach(a => Console.WriteLine(a));
+                Console.WriteLine($"{res.ToList().Count} itens found");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var mainMenu = new CommandMenu("Main");
+            mainMenu.AddCommand("set (?<driver>anglesharp)", SetCommand, "set => set HttpRequest that you want");
+            mainMenu.AddCommand("load (?<url>.*)", LoadCommand, "load => load url or local file");
+            mainMenu.AddCommand("save (?<filename>.*)", SaveCommand, "save => save content to local file");
+            mainMenu.AddCommand("innertext (?<selector>.*)", InnerTextCommand, "innertext => select innertext using css selector");
+            mainMenu.AddCommand("innerhtml (?<selector>.*)", InnerHtmlCommand, "innerhtml => select innerhtml using css selector");
+            mainMenu.AddCommand("links (?<selector>.*)", LinksCommand, "links => select links using css selector");
+            mainMenu.AddCommand("browse", BrowseCommand, "browse => open browser with content");
+            mainMenu.AddCommand("help", HelpCommand, "help => show all commands");
+            mainMenu.AddExitCommand("exit|quit|back", "quit => exit program");
+            mainMenu.PrintHelp();
+            currentMenu = mainMenu;
+            mainMenu.Execute();
         }
 
         static void OpenBrowser(string filename)
